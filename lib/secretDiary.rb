141603fe5d6454cd252f8c1403bdf0c23967513c
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-04-24 09:55:54 +0000

    Refactor to two classes

--- lib/secretDiary.rb
+++ lib/secretDiary.rb
@@ -1,33 +1,73 @@
+# class SecretDiary
+  
+#   attr_reader :entries
+  
+#   def initialize
+#     @entries = []
+#     @open = false
+#   end
+    
+#   def lock
+#     @open = false
+#     #@lock.locked(true)
+#   end
+  
+#   def unlock
+#     @open = true
+#     #@lock.locked(false)
+#   end
+  
+#   def add_entry(new_entry)
+#     raise "Cannot add: secret diary is locked" if @open == false
+#     @entries << new_entry
+#   end
+  
+#   def get_entries
+#     raise "Cannot show entries: secret diary is locked" if @open == false
+#     @entries
+#   end
+  
+#   def open?
+#     @open
+#   end
+
+# end
+
 class SecretDiary
+  def initialize
+    @diary = Diary.new
+    @unlocked = false
+  end
+  
+  def unlock
+    @unlocked = true
+  end
+  
+  def lock 
+    @unlocked = false
+  end
+  
+  def get_entries
+    raise "Diary locked" unless @unlocked
+    @diary.entries
+  end
+  
+  def add_entry(entry)
+    raise "Diary locked" unless @unlocked
+    @diary.entries << entry
+  end
+end
+
+class Diary
   
   attr_reader :entries
   
   def initialize
     @entries = []
-    @open = false
-  end
-    
-  def lock
-    @open = false
-  end
-  
-  def unlock
-    @open = true
-  end
-  
-  def add_entry(new_entry)
-    raise "Cannot add: secret diary is locked" unless @open
-    @entries << new_entry
   end
   
   def get_entries
-    raise "Cannot show entries: secret diary is locked" unless @open
-    p @entries
+    @entries
   end
-  
-  def open?
-    @open
-  end
-  
-  
 end
+  
--- spec/secretDiary_spec.rb
+++ spec/secretDiary_spec.rb
@@ -2,6 +2,7 @@ require "secretDiary"
 
 describe SecretDiary do
   subject(:secretDiary) { described_class.new }
+  let(:diary) { double :diary, :entries => [] }
   
   it "can be locked" do
     expect(secretDiary).to respond_to(:lock)
@@ -11,26 +12,12 @@ describe SecretDiary do
     expect(secretDiary).to respond_to(:unlock)
   end
   
-  it "can accept new entries" do
-    expect(secretDiary).to respond_to(:add_entry).with(1).argument
-  end
-  
-  it "can show all entries" do
-    expect(secretDiary).to respond_to(:get_entries)
-  end
-  
-  describe "#open?" do
-    it "is initially not open" do
-      expect(secretDiary).not_to be_open
-    end
-  end
-  
   describe "#add_entry" do
     
     context "when diary is locked" do
       it "throws error when diary is locked" do
         new_entry = double :new_entry
-        expect { secretDiary.add_entry(new_entry) }.to raise_error "Cannot add: secret diary is locked"
+        expect { secretDiary.add_entry(new_entry) }.to raise_error "Diary locked"
       end
     end
     
@@ -38,7 +25,7 @@ describe SecretDiary do
       it "can accept new entry" do
         new_entry = double :new_entry
         secretDiary.unlock
-        expect(secretDiary.add_entry(new_entry)).to eq([new_entry])
+        expect{secretDiary.add_entry(new_entry)}.not_to raise_error
       end
     end
   end
@@ -47,7 +34,7 @@ describe SecretDiary do
       
     context "when diary is locked" do  
       it "throws error when diary is locked" do
-        expect { secretDiary.get_entries }.to raise_error "Cannot show entries: secret diary is locked"
+        expect { secretDiary.get_entries }.to raise_error "Diary locked"
       end
     end
     
@@ -56,10 +43,17 @@ describe SecretDiary do
         new_entry = double :new_entry  
         secretDiary.unlock
         secretDiary.add_entry(new_entry)
-        expect(secretDiary.get_entries).to eq(secretDiary.entries)
+        expect(secretDiary.get_entries).to include new_entry
       end
     end
   end
   
-  
+end
+
+describe Diary do
+  describe "#get_entries" do
+    it "can show entries" do
+      expect(subject.get_entries).to eq []
+    end
+  end
 end
